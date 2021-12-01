32f442e7f3e6ead56e46aa35714681b3494ce6f8
Alexandr Larionov <alarionov93@yandex.ru>
2021-12-01 16:23:13 +0500

    latest stable v

--- compare.py
+++ compare.py
@@ -4,11 +4,12 @@ Created on Tue Apr 23 13:08:33 2019
 @author: dan
 """
 
-from PIL import Image, ImageOps
+import numpy as np
+import cv2 as cv
 
-SIZE = 16 
+SIZE = 8 
 e = 2.78
-MIN = 0.12
+MIN = 0.001
 
 class NotAnImage(ValueError):
     pass
@@ -20,17 +21,30 @@ def compare(img1, img2):
     D = []
     for x in range(SIZE):
         for y in range(SIZE):
-            a = σ(img1.getpixel((x,y)) / 255)
-            b = σ(img2.getpixel((x,y)) / 255)
+            a = σ(img1[y,x] / 255)
+            b = σ(img2[y,x] / 255)
             D += [(a-b)**2]
 #    print(D)
     return sum(D)**1/2
 
+def compare_new(img1, img2):
+
+    res = np.sum((img1-img2)**2)
+    return res
+
 def getthumb(filename):
     try:
-        return ImageOps.grayscale(Image.open(filename).resize(size = (SIZE,SIZE), resample = Image.HAMMING))
-    except OSError:
-         raise NotAnImage()
+        # print(filename)
+        img = cv.imread(filename, 0)
+        # print(img)
+        res_im = cv.resize(img, (SIZE,SIZE))
+        # print(res_im)
+        # return cv.resize(cv.imread(filename, 0), (SIZE,SIZE))
+        return res_im, (img.shape[1], img.shape[0])
+        # return ImageOps.grayscale(Image.open(filename).resize(size = (SIZE,SIZE), resample = Image.HAMMING))
+    except cv.error as e:
+        print(e)
+        raise NotAnImage()
 
 if __name__ == '__main__':
     try:
@@ -40,7 +54,7 @@ if __name__ == '__main__':
         i1 = getthumb(img1)
         i2 = getthumb(img2)
         x = compare(i1, i2)
-        print("%s <=> %s (%s) %s" % (img1, img2, round(x,2), "да" if x < 0.07 else "похожи" if x < 0.7 else "нет"))
+        print("%s <=> %s (%s) %s" % (img1, img2, round(x,4), "да" if x < 0.07 else "похожи" if x < 0.7 else "нет"))
     except AssertionError:
         print("Usage")
     except FileNotFoundError:
--- dedublicate.py
+++ dedublicate.py
@@ -1,32 +1,73 @@
-from compare import compare, getthumb, MIN, NotAnImage
+from compare import compare, compare_new, getthumb, MIN, NotAnImage
 from shutil import move
 
 import sys
 import os
 
 def scan_dir(_dir):
+    r = []
     for path, dirs, files in os.walk(_dir):
+        if dirs:
+            dirs.pop()
         for f in [ "%s%s" % (path, f) for f in files]:
             try:
-                yield (f, getthumb(f))
+                # print(f)
+                r += [(f, getthumb(f))]
             except NotAnImage:
                 pass
+    return r
     
 def compare_images(scanned, minv):
     checked = []
+    res = []
     for k1, v1 in scanned:
+        print('Checking %s' % k1, end=' ... ', flush=1)
+        nnn = 0
         for k2, v2 in scanned:
             to_check = set([k1,k2])
             if k1 != k2 and to_check not in checked:
-                x = compare(v1, v2)
+                nnn += 1
+                x = compare(v1[0], v2[0])
                 checked += [to_check]
                 if x < minv:
-                    yield (k2, x)
+                    res += [(k1, k2, x)]
+                    
+                    # try:
+                    #     if get_biggest_shape(v1[1], v2[1]) == 1:
+                    #         to_mv = k2
+                    #         print('To_mv=2', v2[1], k2)
+                    #     else:
+                    #         to_mv = k1
+                    #         print('To_mv=1', v1[1], k1)
+                    # except IndexError:
+                    #     print('Check type of scanned elements: should be tuple!')
+                    # res += [(to_mv, x)]
+        print(nnn)
+
+
+    r = sorted(res, key=lambda elem: elem[2])
+    with open('aaa.html', 'w') as f:
+        f.write('<table>\n')
+        for a, b, c in r:
+            f.write('<tr><td><img src=\"%s\" style=\"width: 300px;\"></td><td><img src=\"%s\" style=\"width: 300px;\"></td><td>%s</td></tr>\n' % (a,b,c))
+        f.write('</table>')
+
+    return r
+
+def get_biggest_shape(shape1, shape2):
+    max_w = 0
+    nax_h = 0
+    if shape1[0]*shape1[1] > shape2[0]*shape2[1]:
+        max_val = 1
+    else:
+        max_val = 2
+
+    return max_val
     
 def move_images(compared, _dir):
-    for f, x in compared:
-        ff = "(%s) %s" % (x, os.path.basename(f))
-        print('move(%s, "/tmp/"%s)' % (f, ff), end=' ... ', file=sys.stderr)
+    for f, f2, x in compared:
+        ff = "%s" % os.path.basename(f)
+        # print('move(%s, "./moved/"%s)' % (f, ff), end=' ... ', file=sys.stderr)
         try:
             move(f, _dir+ff)
             print('done', file=sys.stderr)
@@ -35,10 +76,11 @@ def move_images(compared, _dir):
     
 if __name__ == '__main__':
     try:
-        move_images(compare_images(scan_dir(sys.argv[1]), MIN), '/tmp/')
-    except IndexError:
+        move_images(compare_images(scan_dir(sys.argv[1]), MIN), '%s/moved/' % sys.argv[1])
+    except IndexError as e:
+        print(e)
         print(""" USAGE:
-dedublicate /path
+dedublicate /path/
 
 this will move all dublicates of images in path (incl subdirs)
 into /tmp/
